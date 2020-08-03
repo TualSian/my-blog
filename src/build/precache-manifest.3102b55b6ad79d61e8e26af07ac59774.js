c54dc526352978dc66a0e5eaff811c584a246074
sianmuan <mr.sian@yahoo.com>
2020-08-03 12:34:44 +0800

    First commit

---
+++ src/build/precache-manifest.3102b55b6ad79d61e8e26af07ac59774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb2ed0f4e2f56fb3433c7d1bc976e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c24e353caee71b0f17e",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "be6b9e9b9da9c0270974",
+    "url": "/static/js/2.cec87d6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cec87d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c24e353caee71b0f17e",
+    "url": "/static/js/main.51388d28.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
